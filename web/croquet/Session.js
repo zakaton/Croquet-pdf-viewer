bd5b575ac10d1c2aa428d124130d0e2018000b92
Zack <zack@ukaton.com>
2020-04-21 15:11:43 -0700

    wait for pdfViewer to load

--- web/croquet/Session.js
+++ web/croquet/Session.js
@@ -1,9 +1,20 @@
 import Model from "./model/Model.js";
 import View from "./view/View.js";
 
-Croquet.App.root = false;
-Croquet.startSession("croquet-pdf-viewer-4", Model, View)
-    .then(_ => {
-        const {view, model} = _;
-        window.view = view;
+const startSession = () => {
+    Croquet.App.root = false;
+    Croquet.startSession("croquet-pdf-viewer-4", Model, View)
+        .then(_ => {
+            const {view, model} = _;
+            window.view = view;
+        });
+}
+
+if(window.PDFViewerApplication) {
+    startSession();
+}
+else {
+    document.addEventListener('webviewerloaded', event => {
+        startSession();
     });
+}
